dac520dbc48a18a538ec0f5cfbda124394ca64c0
Nivashini0809 <149260141+Nivashini0809@users.noreply.github.com>
2025-02-24 20:44:43 +0530

    Initial commit

---
+++ Digital Marketing Agency/script.js
@@ -0,0 +1,43 @@
+$(document).ready(function() {
+    $('#menu').click(function() {
+        $(this).toggleClass('fa-times');
+        $('.navbar').toggleClass('active');
+    });
+
+    $(window).on('scroll load', function() {
+        $('#menu').removeClass('fa-times');
+        $('.navbar').removeClass('active');
+
+        if ($(window).scrollTop() > 60) {
+            $('.header').addClass('active');
+        } else {
+            $('.header').removeClass('active');
+        }
+
+        $('section').each(function() {
+            let top = $(window).scrollTop();
+            let height = $(this).height();
+            let offset = $(this).offset().top - 200;
+            let id = $(this).attr('id');
+
+            if (top >= offset && top < offset + height) {
+                $('.navbar a').removeClass('active');
+                $('.navbar').find(`[href="#${id}"]`).addClass('active');
+            }
+        });
+
+    });
+
+    $('#read-more-btn').click(function() {
+        $('#read-more').show();
+        $('#read-more-btn').hide();
+        $('#read-less-btn').show();
+    });
+
+    $('#read-less-btn').click(function() {
+        $('#read-more').hide();
+        $('#read-more-btn').show();
+        $('#read-less-btn').hide();
+    });
+ 
+});
